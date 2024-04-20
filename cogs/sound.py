1a108d66132f49b0cb7ac1e53d07aa06c4b843df
RattenPriester <RattenPriester6840@gmail.com>
2024-04-21 01:33:41 +0200

    changed sound.py

--- cogs/sound.py
+++ cogs/sound.py
@@ -1,4 +1,6 @@
 import glob
+import os
+import typing
 import discord
 import asyncio
 
@@ -7,24 +9,40 @@ from discord.ext import commands
 
 
 
-class sound(commands.Cog):
+class psound(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+#TODO fix error (ExtensionFailed: Extension 'cogs.sound' raised an error: TypeError: 'function' object is not subscriptable)
+    async def psound_autocompletion(interaction: discord.Interaction, current: str) -> typing.List[app_commands.choices[str]]:
+        data=[]
+        for v in os.listdir("sounds/*"):
+            if v.endswith(".mp3") or v.endswith(".ogg"):
+                sstr= v.split(".")
+                
+                data.append(app_commands.choices(sstr))
+                
+            return data
 
     @app_commands.command(name="psound", description="plays a sound that you can select")
-    @app_commands.choices(sounds=[])
     @app_commands.describe(sound='the sound you want to play')
-    async def psound(self, ctx: discord.interactions.Interaction, sound: str):
-        voice_channel = ctx.user.voice.channel
+    @app_commands.autocomplete(sounds=psound_autocompletion)
+    async def psound(self, interaction: discord.interactions.Interaction, sound: str):
+        voice_channel = interaction.user.voice.channel
+
+        if voice_channel == None:
+            await interaction.invoke(self._join)
+
         voice_client = await voice_channel.connect()
-        sounds = glob.glob("\sounds*.mp3")
-        source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(sounds))
+        source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(sound))
         voice_client.play(source)
+        interaction.response.send_message(f"playing {sound}")
         while voice_client.is_playing():
             await asyncio.sleep(1)
         await voice_client.disconnect()
-        
+
+
+
 
 async def setup(bot):
-    await bot.add_cog(sound(bot))
+    await bot.add_cog(psound(bot))
